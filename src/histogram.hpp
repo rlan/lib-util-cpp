748783484fddeef989311bd7a3234c9e1f00c5f2
Rick Lan <rlan@users.noreply.github.com>
2018-04-20 17:00:36 +0900

    Histogram - add name and self-report in the destructor

--- src/histogram.hpp
+++ src/histogram.hpp
@@ -2,6 +2,8 @@
 #define HISTOGRAM_HPP_
 
 #include <ostream>
+#include <string>
+#include <vector>
 
 /**
  * Tracks histogram from 0 to n_bins-1. Values >= n_bins-1 are added to "n_bins-1".
@@ -9,11 +11,14 @@
 class UIntHistogram
 {
   public:
-    UIntHistogram(size_t n_bins)
+    UIntHistogram(size_t n_bins, const std::string &name = "")
     : count_(n_bins)
+    , name_(name)
     { }
     ~UIntHistogram()
-    { }
+    {
+      std::cout << *this;
+    }
 
     void push(size_t val)
     {
@@ -26,18 +31,22 @@ class UIntHistogram
 
   protected:
     std::vector<size_t> count_;
+    const std::string name_;
   private:
     UIntHistogram();
 };
 
 std::ostream& operator<<(std::ostream& os, const UIntHistogram& obj)
 {
+  if (obj.name_.size())
+    os << obj.name_ << std::endl;
   for (size_t i = 0; i < obj.count_.size(); i++) {
     os << i << ":" << obj.count_[i] << " ";
     if ((i+1) % 10 == 0)
       os << std::endl;
   }
   os << std::endl;
+  return os;
 }
 
 #endif // HISTOGRAM_HPP_
